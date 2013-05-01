b5dadd02134ae8e6810823a00677eab647da18c9
hilaryjoyce <hilary.paulin@gmail.com>
2013-05-01 11:32:41 -0400

    Added tss_force.py for RDF type only, along with the RDF specific module.

---
+++ Tss_force_code/RDF_pulling.py
@@ -0,0 +1,49 @@
+''' RDF_pulling.py contains all the functions that load, manipulate, and save a single 
+    Ramp, Defl, Force 3-column file as a 2-column file for tss-force.
+    Also contains function to save calculate all for single RDF folder.
+
+    Parameters required:
+        - name of RDF file 'fileName'
+        - location of data set 'folder'
+''' 
+
+def curveNumFinder(fileName):
+    ''' Takes a filename and assuming XXX.txt format, 
+        returns the curve number XXX as a string.
+        Aborts and returns error if XXX is not a number.'''
+    import re
+    curve =  re.search('[a-z]?[0-9]{3}(?=.txt)', fileName)
+    curveNum = curve.group(0)
+    return curveNum
+
+def loadRDF(fileName):
+    ''' Takes a standard SMFS file with ramp, force, and deflection data
+        and returns ramp, force, and deflection arrays'''
+    ramp = np.zeros(6144) # in nm
+    defl = np.zeros(6144) # in nm
+    force = np.zeros(6144) # in pN
+    file = open(fileName, 'r')
+    file.readline()
+    i = 0
+    for line in file:
+        ramp[i] = float(line.split()[0])
+        defl[i] = float(line.split()[1])
+        force [i] = float(line.split()[2])
+        i = i + 1
+    file.close
+    return ramp, defl, force
+
+def tss_force(fileName):
+    ''' Takes ramp, force and deflection arrays and 
+        calculates the aligned and zeroed tss (nm) and force (pN)'''
+    ramp, defl, force = loadRDF(fileName)
+    tss = (ramp-defl)*-1+400
+    force = (force-np.average(force[5837:6144]))*-1
+    
+    zero=0
+    while (force[zero]<0):
+        zero=zero+1
+    
+    tss =tss-tss[zero]
+    return [tss, force]
+
---
+++ Tss_force_code/tss_force.py
@@ -0,0 +1,49 @@
+''' tss_force.py contains the output functions to save tss-force data. 
+    Parameters:
+        - folder of raw data
+        - type = ['RDF' or 'ADR']
+
+    Currently only written for 'RDF' type.
+'''
+
+def tss_force_list(folder, type):
+    ''' Takes a folder of raw data and a type ('RDF' or 'ADR') and
+        returns a list of paired tss-force arrays and a second list of the
+        curve numbers for each file.'''
+    from glob import glob
+    if type == 'RDF':
+        import RDF_pulling as pull
+        
+        RDF_folder = '%s/RDF_text/'
+        RDF_files = glob('%s*.txt' % RDF_folder)
+        
+        tss_force_list = []
+        curve_number_list = []
+        for file in RDF_files:
+            curve_number_list.append(pull.curveNumFinder(file))
+            tss_force_list.append(pull.tss_force(file))
+
+    return tss_force_list, curve_number_list
+
+def saveFile2(path, col1, col2, col1_name, col2_name):
+    '''Saves a file with 2 columns of data.'''
+    file = open(path, 'w')
+    file.write("%s\t%s\n" % (col1_name, col2_name))
+    k=0
+    while k<len(col1):
+        el = '%3g\t%3g\n' % (col1[k], col2[k])
+        file.write(el)
+        k=k+1
+    file.close
+
+def saveTssForce(folder, type):
+    ''' Save a .txt file of tss and force to Tss_force_data folder.
+        Should only be executed once for all files in
+        a given experiment or it's rather redundant.'''
+        tss_force_list, curve_number_list = tss_force_list(folder, type)
+        i = 0
+        while i < len(curve_number_list):
+            filename = "%s/Tss_Force_data/tss_force_%s.txt" % (folder, curve_number_list[i])
+            saveFile2(filename, tss_force_list[i][0], tss_force_list[i][1], "Tss (nm)", "Force (pN)")
+            i = i+1
+
