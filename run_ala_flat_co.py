258e57157c93031edaadf9a98a2a286766d1e70d
hilaryjoyce <hilary.paulin@gmail.com>
2013-05-22 10:51:13 -0400

    Lots of small changes to clustering plotting to get the right graphs.

--- run_ala_flat_co.py
+++ run_ala_flat_co.py
@@ -2,6 +2,6 @@
 
 from Coincidence.coincidence import saveMultipleCoincidence
 
-param_folder = '../Data/aLa_flat_all/Transform_analysis/Parameters_p0.4_minTss5_minForce45/'
+param_folder = '../Data/aLa_flat_all/Transform_analysis/Parameters_p0.4_minTss8_minForce45/'
 
 saveMultipleCoincidence(param_folder)
